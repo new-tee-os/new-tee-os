9bc0e71a1e805088d4922f9c8e2ce8ec20964edc
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-05-28 13:29:19 +0800

    Cleanup code

--- elfloader/src/lib.rs
+++ elfloader/src/lib.rs
@@ -1,13 +1,7 @@
-//#[cfg(feature="elf64")]
-// #[derive(Debug)]
 pub mod elfloader;
 
-
 #[test]
-fn test1() {//riscv-hello-world
+fn test1() {
     use elfloader::load_elf64_riscv;
-    load_elf64_riscv("./as");
-//     let a:[i32;3]=[1,2,3];
-//     assert_eq!(a[..2],[1,2]);
+    load_elf64_riscv("./riscv-hello-world");
 }
-

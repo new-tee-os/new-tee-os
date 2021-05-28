3376abbc4fd2f9e80a423c42f9345f0428225c67
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-05-28 14:37:54 +0800

    OOP-ify code

--- elfloader/src/elfloader.rs
+++ elfloader/src/elfloader.rs
@@ -1,8 +1,7 @@
 use goblin::elf;
 use goblin::elf::{header::*, program_header::*};
 
-use std::alloc::{alloc, alloc_zeroed, dealloc, Layout};
-use std::fs;
+use std::alloc::{alloc, Layout};
 
 const PAGE_SIZE: usize = 0x1000;
 
@@ -16,81 +15,57 @@ fn get_pages(n: u64) -> usize {
     }
 }
 
-fn check_elf64_riscv(head: &Header) -> Result<bool, bool> {
-    //should assert_eq!
-    if head.e_ident[0] != 0x7fu8 {
-        return Result::Err(false);
-    } else if head.e_machine != EM_RISCV || head.e_type != ET_EXEC {
-        return Result::Err(false);
+fn check_elf64_riscv(head: &Header) {
+    if &head.e_ident[0..4] != b"\x7FELF" {
+        panic!("invalid ELF magic number: {:?}", &head.e_ident[0..4]);
     }
-    return Result::Ok(true);
+    if head.e_machine != EM_RISCV || head.e_type != ET_EXEC {
+        panic!("unsupported architecture or ELF file type")
+    }
+    // check pass
 }
 
-//return the stack pointer
-fn prepare_libc_args() -> usize {
-    //give one page to the stack
-    let mem: *mut [u8];
-    unsafe {
-        let _mem: *mut u8 = alloc_zeroed(
-            Layout::from_size_align(PAGE_SIZE, 32) //4*word/byte??????
-                .unwrap(),
-        );
-
-        mem = core::slice::from_raw_parts_mut(_mem, get_pages(PAGE_SIZE as _) << 12);
-    }
-    let stack_addr = 0xBF800100;
-    //create_mapping(mem as *mut [u8],stack_addr); //is the stack position determined by this?
-    let argc: usize = 0;
-    let argv: [usize; 0];
-    //0
-    let env: [usize; 0];
-    //0
-    let aux: [usize; 36];
-    //0
-    //strings
-    let offset = (42 / 32 + 1) * 4 * 8;
-
-    stack_addr + PAGE_SIZE - offset
+pub struct ElfFile {
+    entry: u64,
 }
 
-pub fn load_elf64_riscv(path: &str) {
-    let data = fs::read(path).expect("file not exist");
-    let bin = elf::Elf::parse(&data).expect("parse failed");
+impl ElfFile {
+    pub fn load(data: &[u8]) -> ElfFile {
+        let elf = elf::Elf::parse(data).expect("parse failed");
+        check_elf64_riscv(&elf.header);
 
-    println!("{:?}", bin.header);
-    for seg in bin.program_headers.iter() {
-        if seg.p_type == PT_LOAD {
-            println!("{:?}", seg);
-            // println!("{:?}",String::from(&bin.shdr_strtab[seg.sh_name]));
-        }
-    }
+        for seg in elf.program_headers.iter() {
+            if seg.p_type == PT_LOAD {
+                let mem = unsafe {
+                    let size = get_pages(seg.p_memsz) * PAGE_SIZE;
+                    //allocate physical memory
+                    //set one page for each of the segment, for it requires much more support from OS
+                    //if we map one virt.
+                    let mem_ptr: *mut u8 = alloc(Layout::from_size_align(size, PAGE_SIZE).unwrap());
 
-    for seg in bin.program_headers.iter() {
-        if seg.p_type == PT_LOAD {
-            let mem: &mut [u8];
-            unsafe {
-                //allocate physical memory
-                //set one page for each of the segment, for it requires much more support from OS
-                //if we map one virt.
-                let _mem: *mut u8 =
-                    alloc(Layout::from_size_align(seg.p_memsz as usize, PAGE_SIZE).unwrap());
+                    core::slice::from_raw_parts_mut(mem_ptr, size)
+                };
 
-                mem = core::slice::from_raw_parts_mut(_mem, get_pages(seg.p_memsz) * PAGE_SIZE);
+                // the virtual address where `mem` will be placed
+                let load_addr = (seg.p_vaddr as usize) / PAGE_SIZE * PAGE_SIZE;
+                let virt_off_begin = (seg.p_vaddr as usize) - load_addr;
+                let virt_off_end = virt_off_begin + (seg.p_filesz as usize);
+                let file_begin = seg.p_offset as usize;
+                let file_end = file_begin + (seg.p_filesz as usize);
+                mem[virt_off_begin..virt_off_end].copy_from_slice(&data[file_begin..file_end]);
+
+                //add these phy memories to the vm_area tree, and map page table
+                //create_mapping(mem as *mut [u8],seg.p_vaddr);
             }
-            // the virtual address where `mem` will be placed
-            let load_addr = (seg.p_vaddr as usize) / PAGE_SIZE * PAGE_SIZE;
-            let virt_off_begin = (seg.p_vaddr as usize) - load_addr;
-            let virt_off_end = virt_off_begin + (seg.p_filesz as usize);
-            let file_begin = seg.p_offset as usize;
-            let file_end = file_begin + (seg.p_filesz as usize);
-            mem[virt_off_begin..virt_off_end].copy_from_slice(&data[file_begin..file_end]);
+        }
 
-            //add these phy memories to the vm_area tree, and map page table
-            //create_mapping(mem as *mut [u8],seg.p_vaddr);
+        ElfFile {
+            entry: elf.header.e_entry,
         }
     }
 
-    let sp = prepare_libc_args();
-    let pc = bin.header.e_entry;
-    //satp——页基寄存器
+    #[inline]
+    pub fn entry(&self) -> u64 {
+        self.entry
+    }
 }
--- elfloader/src/lib.rs
+++ elfloader/src/lib.rs
@@ -2,6 +2,5 @@ pub mod elfloader;
 
 #[test]
 fn test1() {
-    use elfloader::load_elf64_riscv;
-    load_elf64_riscv("./riscv-hello-world");
+    elfloader::ElfFile::load(&std::fs::read("./riscv-hello-world").unwrap());
 }

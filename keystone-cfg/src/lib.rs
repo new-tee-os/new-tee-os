f1ce5bbc6a36082268732e6c0fc8e520801497a1
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-05-31 20:17:08 +0800

    Purge preallocated page table

--- keystone-cfg/src/lib.rs
+++ keystone-cfg/src/lib.rs
@@ -13,6 +13,6 @@ pub const KERNEL_UTM_BASE: usize = 0xFFFF_FFFF_8000_0000;
 pub const EPM_SIZE: usize = 0x100_000;
 pub const UTM_SIZE: usize = 0x1_000;
 pub const EDGE_BUFFER_SIZE: usize = 3 << 10;
-pub const KERNEL_EPM_OFFSET: usize = 0x10_000;
+pub const KERNEL_EPM_OFFSET: usize = 0x4_000;
 
 // KERNEL_EPM_OFFSET + KERNEL_SIZE must be *smaller* than EPM_SIZE

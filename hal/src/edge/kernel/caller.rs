04285131a3c1840c9da2a6735af998c79f3204a9
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-06-04 23:02:45 +0800

    Add docs for `EdgeCaller`

--- hal/src/edge/kernel/caller.rs
+++ hal/src/edge/kernel/caller.rs
@@ -1,16 +1,26 @@
 use crate::edge::EdgeMemory;
 
+/// The edge caller trait, which provides access to edge memory and allows the
+/// user to issue edge calls.
+///
+/// In order to prevent concurrent access to edge callers, this object holds
+/// a mutex provided by `GlobalEdgeCaller`. When the edge caller object gets
+/// dropped, the mutex will be unlocked.
 pub trait EdgeCaller {
     fn edge_mem(&mut self) -> &mut EdgeMemory;
     unsafe fn edge_call(&self);
 }
 
+/// The global edge caller trait, which provides access to the actual `EdgeCaller`.
+///
+/// This object guarantees that edge calls are not concurrent by using a mutex.
 pub trait GlobalEdgeCaller<'c, C: EdgeCaller + 'c>: Sync {
     fn acquire(&'c self) -> C;
 }
 
 use crate::sys::GLOBAL_EDGE_CALLER;
 
+/// Acquire the edge caller and do something with it (usually issuing edge calls).
 pub fn with_edge_caller<F, V>(f: F) -> V
 where
     F: FnOnce(&mut dyn EdgeCaller) -> V,
--- hal/src/sys/keystone/rt/edge.rs
+++ hal/src/sys/keystone/rt/edge.rs
@@ -4,9 +4,29 @@ use super::sbi;
 use crate::cfg::KERNEL_UTM_BASE;
 use crate::edge::{EdgeCaller, EdgeMemory, GlobalEdgeCaller};
 
-pub struct KsEdgeCallerHolder<'l>(MutexGuard<'l, ()>);
+/// The Keystone global edge caller, which provides access to `KsEdgeCallerHolder`
+/// and prevents concurrent edge calls by using a mutex.
+///
+/// In Rust, it is represented by a `Mutex` holding some phantom data.
+///
+/// This struct must be marked public (in order to compile successfully).
 pub struct KsGlobalEdgeCaller(Mutex<()>);
 
+/// The Keystone edge caller holder, which provides unrestricted access to
+/// edge memory and allows the user to issue edge calls.
+///
+/// It is returned by `KsGlobalEdgeCaller` and holds the mutex's lock.
+/// Therefore, the object's holder will be the only one able to issue
+/// edge calls when this object is alive.
+///
+/// When this object gets dropped, the mutex will be unlocked, thus
+/// allowing others to acquire the edge caller again.
+///
+/// This struct must be marked public (in order to compile successfully).
+pub struct KsEdgeCallerHolder<'l>(MutexGuard<'l, ()>);
+
+/// Export the global edge caller to `crate::edge`. This must be marked
+/// public (in order to compile successfully).
 pub static GLOBAL_EDGE_CALLER: KsGlobalEdgeCaller = KsGlobalEdgeCaller(Mutex::new(()));
 
 const EDGE_MEM_BASE: *mut EdgeMemory = KERNEL_UTM_BASE as _;

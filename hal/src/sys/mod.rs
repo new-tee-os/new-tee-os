03f43f3e40acdaa41dd54b8c5d5def0e8bbb59cf
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-05 15:42:07 +0800

    Port `hal` to `x86-vm`

--- hal/src/sys/mod.rs
+++ hal/src/sys/mod.rs
@@ -3,6 +3,9 @@ cfg_if::cfg_if! {
     if #[cfg(feature = "keystone")] {
         mod keystone;
         pub use keystone::*;
+    } else if #[cfg(feature = "x86-vm")] {
+        mod x86_vm;
+        pub use x86_vm::*;
     } else {
         compile_error!("unsupported platform configuration");
     }
---
+++ hal/src/sys/x86_vm/mod.rs
@@ -0,0 +1,3 @@
+// stub
+
+pub mod cfg {}

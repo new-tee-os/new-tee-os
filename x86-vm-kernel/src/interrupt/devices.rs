ad9e2295b03168ec278b95e990dda665e6a20411
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-08 22:32:03 +0800

    Add end-of-interrupt

--- x86-vm-kernel/src/interrupt/devices.rs
+++ x86-vm-kernel/src/interrupt/devices.rs
@@ -1,10 +1,14 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-use super::pic::InterruptIndex;
+use super::pic::{InterruptIndex, PICS};
 use crate::dbg_print;
 
 extern "x86-interrupt" fn timer_int(_stack_frame: InterruptStackFrame) {
     dbg_print!(".");
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
 }
 
 pub fn load_idt_entries(idt: &mut InterruptDescriptorTable) {

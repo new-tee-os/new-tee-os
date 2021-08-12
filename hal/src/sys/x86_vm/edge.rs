97e85f1dfa04fb3a3810e4a1b2e24e97b77e5cbc
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-12 16:41:42 +0800

    Implement `QemuEdgeStream`

--- hal/src/sys/x86_vm/edge.rs
+++ hal/src/sys/x86_vm/edge.rs
@@ -1,9 +1,11 @@
 use spin::{Mutex, MutexGuard};
+use uart_16550::SerialPort;
 
-use crate::edge::{EdgeCallerHolder, EdgeMemory, GlobalEdgeCaller};
+use crate::edge::{EdgeCallerHolder, EdgeMemory, EdgeStream, GlobalEdgeCaller};
 
 pub struct QemuGlobalEdgeCaller(Mutex<EdgeMemory>);
 pub struct QemuEdgeCallerHolder<'l>(MutexGuard<'l, EdgeMemory>);
+struct QemuEdgeStream<'l>(MutexGuard<'l, SerialPort>);
 
 pub static GLOBAL_EDGE_CALLER: QemuGlobalEdgeCaller =
     QemuGlobalEdgeCaller(Mutex::new(EdgeMemory::new()));
@@ -22,6 +24,21 @@ impl EdgeCallerHolder for QemuEdgeCallerHolder<'_> {
     }
 
     unsafe fn edge_call(&mut self) {
-        todo!()
+        let edge_mem = self.edge_mem();
+        let mut edge_stream = QemuEdgeStream(crate::arch::x86_vm::qemu::SERIAL_EDGE.lock());
+        // write request
+        edge_mem.serialize(&mut edge_stream);
+        // read result
+        edge_mem.deserialize(&mut edge_stream);
+    }
+}
+
+impl EdgeStream for QemuEdgeStream<'_> {
+    fn read(&mut self) -> u8 {
+        self.0.receive()
+    }
+
+    fn write(&mut self, ch: u8) {
+        self.0.send(ch);
     }
 }

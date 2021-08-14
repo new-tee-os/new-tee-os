baab47bd4e15cc440cbbca9e32206de613c1abf8
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-14 11:49:35 +0800

    Add user mode segment selectors

--- x86-vm-kernel/src/interrupt/gdt.rs
+++ x86-vm-kernel/src/interrupt/gdt.rs
@@ -6,7 +6,9 @@ use x86_64::{
 
 pub const KERNEL_CODE_SEL: SegmentSelector = SegmentSelector::new(1, PrivilegeLevel::Ring0);
 pub const KERNEL_DATA_SEL: SegmentSelector = SegmentSelector::new(2, PrivilegeLevel::Ring0);
-pub const TSS_SEL: SegmentSelector = SegmentSelector::new(3, PrivilegeLevel::Ring0);
+pub const USER_CODE_SEL: SegmentSelector = SegmentSelector::new(3, PrivilegeLevel::Ring3);
+pub const USER_DATA_SEL: SegmentSelector = SegmentSelector::new(4, PrivilegeLevel::Ring3);
+pub const TSS_SEL: SegmentSelector = SegmentSelector::new(5, PrivilegeLevel::Ring0);
 
 lazy_static! {
     pub static ref GDT: GlobalDescriptorTable = {
@@ -18,6 +20,14 @@ lazy_static! {
         assert_eq!(
             gdt.add_entry(Descriptor::kernel_data_segment()),
             KERNEL_DATA_SEL,
+        );
+        assert_eq!(
+            gdt.add_entry(Descriptor::user_code_segment()),
+            USER_CODE_SEL,
+        );
+        assert_eq!(
+            gdt.add_entry(Descriptor::user_data_segment()),
+            USER_DATA_SEL,
         );
         assert_eq!(
             gdt.add_entry(Descriptor::tss_segment(&super::tss::TSS)),

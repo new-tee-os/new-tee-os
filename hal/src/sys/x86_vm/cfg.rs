437f0af192e78d76682d0323507ab92d35e578f8
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-14 14:28:48 +0800

    Move kernel stack to the higher half

--- hal/src/sys/x86_vm/cfg.rs
+++ hal/src/sys/x86_vm/cfg.rs
@@ -2,6 +2,7 @@ pub const PAGE_SIZE: usize = 0x1_000;
 
 // also defined in the target configuration JSON
 pub const KERNEL_BASE: usize = 0xFFFF_FFFF_0000_0000;
+pub const KERNEL_STACK_BASE: usize = 0xFFFF_FFFF_FFF8_0000;
 // also defined in `x86-vm-kernel/Cargo.toml`
 // 3 GiB is not sufficient for QEMU
 pub const KERNEL_MIRROR_BASE: usize = 0xFFFF_FFF0_0000_0000;

fad91e55ad7655f4f112fa8c543f7cc52a5e50cf
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-14 13:51:43 +0800

    Adjust segment selector orders in correspondance to `syscall` and `sysret` convention

--- x86-vm-kernel/src/interrupt/gdt.rs
+++ x86-vm-kernel/src/interrupt/gdt.rs
@@ -6,13 +6,14 @@ use x86_64::{
 
 pub const KERNEL_CODE_SEL: SegmentSelector = SegmentSelector::new(1, PrivilegeLevel::Ring0);
 pub const KERNEL_DATA_SEL: SegmentSelector = SegmentSelector::new(2, PrivilegeLevel::Ring0);
-pub const USER_CODE_SEL: SegmentSelector = SegmentSelector::new(3, PrivilegeLevel::Ring3);
-pub const USER_DATA_SEL: SegmentSelector = SegmentSelector::new(4, PrivilegeLevel::Ring3);
+pub const USER_DATA_SEL: SegmentSelector = SegmentSelector::new(3, PrivilegeLevel::Ring3);
+pub const USER_CODE_SEL: SegmentSelector = SegmentSelector::new(4, PrivilegeLevel::Ring3);
 pub const TSS_SEL: SegmentSelector = SegmentSelector::new(5, PrivilegeLevel::Ring0);
 
 lazy_static! {
     pub static ref GDT: GlobalDescriptorTable = {
         let mut gdt = GlobalDescriptorTable::new();
+        // note: the following order is enforced by `syscall` and `sysret` calling conventions
         assert_eq!(
             gdt.add_entry(Descriptor::kernel_code_segment()),
             KERNEL_CODE_SEL,
@@ -22,12 +23,12 @@ lazy_static! {
             KERNEL_DATA_SEL,
         );
         assert_eq!(
-            gdt.add_entry(Descriptor::user_code_segment()),
-            USER_CODE_SEL,
-        );
-        assert_eq!(
             gdt.add_entry(Descriptor::user_data_segment()),
             USER_DATA_SEL,
+        );
+        assert_eq!(
+            gdt.add_entry(Descriptor::user_code_segment()),
+            USER_CODE_SEL,
         );
         assert_eq!(
             gdt.add_entry(Descriptor::tss_segment(&super::tss::TSS)),

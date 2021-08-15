33113209c1e957af432e25e3b9def622624e2b46
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-15 15:57:18 +0800

    Add missing constants

--- hal/src/sys/x86_vm/cfg.rs
+++ hal/src/sys/x86_vm/cfg.rs
@@ -6,3 +6,8 @@ pub const KERNEL_STACK_BASE: usize = 0xFFFF_FFFF_FFF8_0000;
 // also defined in `x86-vm-kernel/Cargo.toml`
 // 3 GiB is not sufficient for QEMU
 pub const KERNEL_MIRROR_BASE: usize = 0xFFFF_FFF0_0000_0000;
+
+pub const USER_BASE: usize = 0x40_0000;
+// in Linux, this is one byte past the last address in userspace
+// (not a canonical virtual address in fact)
+pub const USER_STACK_TOP: usize = 0x8000_0000_0000;

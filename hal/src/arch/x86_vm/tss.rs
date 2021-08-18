bb8b58d77e558f898356bb25ede2abfce6b5a2cd
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-18 15:56:49 +0800

    Add privilege stack in TSS

--- hal/src/arch/x86_vm/tss.rs
+++ hal/src/arch/x86_vm/tss.rs
@@ -8,9 +8,18 @@ lazy_static! {
     pub static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
         tss.interrupt_stack_table[EMERGENCY_IST_INDEX as usize] = {
+            // an emergency stack used at double faults
             static mut EMERGENCY_STACK: [u8; EMERGENCY_STACK_SIZE] = [0; EMERGENCY_STACK_SIZE];
 
             let stack_start = VirtAddr::from_ptr(unsafe { &EMERGENCY_STACK });
+            let stack_end = stack_start + EMERGENCY_STACK_SIZE;
+            stack_end
+        };
+        tss.privilege_stack_table[0] = {
+            // no cascaded interrupt here, so one stack per CPU is sufficient
+            static mut KERNEL_INTR_STACK: [u8; EMERGENCY_STACK_SIZE] = [0; EMERGENCY_STACK_SIZE];
+
+            let stack_start = VirtAddr::from_ptr(unsafe { &KERNEL_INTR_STACK });
             let stack_end = stack_start + EMERGENCY_STACK_SIZE;
             stack_end
         };
